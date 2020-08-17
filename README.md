1fabeb07b6f0e2da93a92b3b104bbc0806156287
= <aam.himel@gmail.com>
2020-08-17 14:07:30 +0600

    final commit and ready to be submit

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # hard-rock-solution
-api link: https://api.lyrics.ovh/suggest/summer
+
+## Description
+
+In this project we used an online Lyrics searching api. We can find any song lyrics by searching with the Song name or Artist name. It was fun to build this project.
+
+## Resources
+
+Song api : [Here is the API for finding the songs using any Search params](https://api.lyrics.ovh/suggest/alan%20walker);
+
+Lyrics Api : [Here is the API for finding the Lyrics of the song](https://api.lyrics.ovh/v1/alan%20walker/faded);
+
+NOTE:
+You need to get Artist and Track title to call the lyrics api.
+
+Api Docs : https://lyricsovh.docs.apiary.io/#reference/0/lyrics-of-a-song/search
--- main.js
+++ main.js
@@ -4,37 +4,78 @@ console.log('Initializing js....');
 
 const search = document.getElementById('searchText');
 const searchBtn = document.getElementById('btn-search');
-let url = "https://api.lyrics.ovh/suggest/";
-searchBtn.addEventListener('click', getQueryText)
+const author = document.getElementById('author');
+const lyric = document.getElementById('lyric');
+const singleResult = document.querySelector('.single-result');
+const searchResult = document.querySelector('.search-result');
+const lyricTitle = document.getElementById('lyric-title');
 
-// function to get query text
-function getQueryText() {
+
+searchBtn.addEventListener('click', CallLyricsApi)
+
+// function to call the api
+function CallLyricsApi() {
     const searchText = search.value;
     console.log(searchText);
 
-    fetch(`https://api.lyrics.ovh/suggest/${searchText}`)
+    const songList = fetch(`https://api.lyrics.ovh/suggest/${searchText}`)
         .then(res => res.json())
         .then(data => {
             // console.log(data.data[0]);
             printData(data.data);
+            return data.data;
         })
 
     search.value = '';
 }
 
-
+// single track info
 function printData(data) {
     for (i = 0; i < 10; i++) {
         console.log(data[i]);
-        let { title, artist: { name } } = data[i];
-        displayLyrics(name, title);
+        let { title, id, artist: { name } } = data[i];
+
+        const getLyricsBtn = document.createElement('button');
+        const p = document.createElement('p');
+
+
+        getLyricsBtn.className = "btn btn-success ml-2 get-lyrics";
+        getLyricsBtn.innerText = "Get Lyrics!";
+        getLyricsBtn.id = `${id}`;
+        p.innerHTML = `<strong>${title}</strong> Album By ${name}`;
+        p.className = "author lead";
+        p.appendChild(getLyricsBtn);
+        author.appendChild(p);
+
+        const card = document.createElement('div');
+        card.innerHTML = `<div class="single-result row align-items-center my-3 p-3 get-lyrics" id="singleResult">
+                                <div class="col-md-9">
+                                    <h3 class="lyrics-name">${title}</h3>
+                                    <p class="author lead">Album by <span>${name}</span></p>
+                                </div>
+                                <div class="col-md-3 text-md-right text-center">
+                                    <button class="btn btn-success get-lyrics-card" id="${id}">Get Lyrics</button>
+                                </div>
+                         </div>`
+
+
+        searchResult.appendChild(card);
+        getLyricsBtn.addEventListener('click', event => { displayLyrics(name, title); });
+
     }
+
 }
 
+
+// Display lyrics into the UI
 function displayLyrics(artist, title) {
     fetch(`https://api.lyrics.ovh/v1/${artist}/${title}`)
         .then(res => res.json())
         .then(data => {
+            const songLyric = data.lyrics;
+            lyricTitle.innerText = title;
+            lyric.innerText = songLyric;
             console.log(data.lyrics);
         })
 }
+
